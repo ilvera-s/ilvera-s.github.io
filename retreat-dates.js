68555a9d497d88c16862fd76f63b9ee6d22c7793
Ilvera Sitdikova <ilvera69@mail.ru>
2026-04-17 16:03:24 +0500

    Add files via upload

---
+++ retreat-dates.js
@@ -0,0 +1,21 @@
+// Edit only this file to update retreat dates on the public pages.
+window.RETREAT_DATES = {
+    family: {
+        heading: "Ретрит «Сила рода»",
+        dates: [
+            "С 1 по 3 мая",
+            "С 8 по 10 мая"
+        ],
+        additionalInfo: "",
+        contactText: "Следующие даты проведения ретритов можно согласовать по телефону: +79173482348"
+    },
+    masters: {
+        heading: "Ретрит для мастеров «Познай свою силу»",
+        dates: [
+            "С 13 по 17 августа - мест нет",
+            "С 15 по 18 мая"
+        ],
+        additionalInfo: "",
+        contactText: "Следующие даты проведения ретритов можно согласовать по телефону: +79173482348"
+    }
+};
